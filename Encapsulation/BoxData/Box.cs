e23e66f9fc2a70c8da5b7e681f0c751ab7d6ea24
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2020-07-19 23:23:47 +0300

    Added BoxData from Encapsulation

---
+++ Encapsulation/BoxData/Box.cs
@@ -0,0 +1,90 @@
+﻿namespace BoxData
+{
+    public class Box
+    {
+        private const string InvalidSideMessage = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                ValidateSide(value, nameof(this.Length));
+
+                this.length = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                ValidateSide(value, nameof(this.Width));
+
+                this.width = value;
+            }
+        }
+
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                ValidateSide(value, nameof(this.Height));
+
+                this.height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            double surfaceArea = 2 * (this.length * this.width) + 2 * (this.length * this.height) + 2 * (this.width * this.height);
+            return surfaceArea;
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * (this.length * this.height) + 2 * (this.width * this.height);
+            return lateralSurfaceArea;
+        }
+
+        public double CalculateVolume()
+        {
+            double volume = this.length * this.width * this.height;
+            return volume;
+
+        }
+        private void ValidateSide(double value, string sideName)
+        {
+            if (value <= 0)
+            {
+                //Console.WriteLine(string.Format(InvalidSideMessage, sideName));
+                throw new System.ArgumentException(string.Format(InvalidSideMessage, sideName));
+            }
+
+        }
+    }
+}
---
+++ Encapsulation/BoxData/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, heigth);
+
+                double boxSurfaceArea = box.CalculateSurfaceArea();
+                double boxLateralSurfaceArea = box.CalculateLateralSurfaceArea();
+                double boxVolume = box.CalculateVolume();
+
+                Console.WriteLine($"Surface Area - {boxSurfaceArea:f2}");
+                Console.WriteLine($"Lateral Surface Area - {boxLateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {boxVolume:f2}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}

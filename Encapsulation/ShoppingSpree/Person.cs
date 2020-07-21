a49e299558c45a6eb0b5640662eceb6b8af4bb9e
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2020-07-21 23:35:51 +0300

    added proj

---
+++ Encapsulation/ShoppingSpree/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+
+        private List<Product> bagOfProducts;
+
+        private Person()
+        {
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public Person(string name, decimal money) : this()
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Bag => this.bagOfProducts;
+
+        public void AddProduct(Product product)
+        {
+            if (this.Money < product.Cost)
+            {
+                throw new InvalidOperationException($"{this.Name} can't afford {product.Name}");
+            }
+
+            this.Money -= product.Cost;
+
+            this.bagOfProducts.Add(product);
+        }
+
+        public override string ToString()
+        {
+            string productsOutput = this.Bag.Count > 0 ?
+                String.Join(", ", this.Bag) : "Nothing bought";
+
+            return $"{this.Name} - {productsOutput}";
+        }
+
+    }
+}
---
+++ Encapsulation/ShoppingSpree/StartUp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> peopleList = new List<Person>();
+            List<Product> productsList = new List<Product>();
+
+            var people = Console.ReadLine().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var products = Console.ReadLine().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            try
+            {
+                foreach (var personkvp in people)
+                {
+                    var token = personkvp.Split("=", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    var name = token[0];
+                    var money = decimal.Parse(token[1]);
+                    var person = new Person(name, money);
+
+                    peopleList.Add(person);
+                }
+
+
+                foreach (var productKvp in products)
+                {
+                    var token = productKvp.Split("=", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    var name = token[0];
+                    var money = decimal.Parse(token[1]);
+                    var product = new Product(name, money);
+
+                    productsList.Add(product);
+                }
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+                Environment.Exit(0);
+            }
+
+
+            BuyProducts(peopleList, productsList);
+
+
+            foreach (var person in peopleList)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+        }
+
+        private static void BuyProducts(List<Person> peopleList, List<Product> productsList)
+        {
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string personName = commandArgs[0];
+                string productName = commandArgs[1];
+
+                try
+                {
+                    Person person = peopleList.FirstOrDefault(p => p.Name == personName);
+                    Product product = productsList.FirstOrDefault(p => p.Name == productName);
+
+                    person.AddProduct(product);
+
+                    Console.WriteLine($"{person.Name} bought {product.Name}");
+
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    Console.WriteLine(ioe.Message);
+                }
+            }
+        }
+    }
+}
